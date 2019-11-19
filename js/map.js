159028cc57e5a548720a253dc791d64fd094f932
saniok2525 <saniok2525@gmail.com>
2019-11-19 16:10:10 +0000

    DTL2

--- js/map.js
+++ js/map.js
@@ -1,9 +1,6 @@
 function initMap() {
-    // The location of Uluru
-    var uluru = {lat: 49.773, lng: 24.029};
-    // The map, centered at Uluru
+    var lviv = {lat: 49.773, lng: 24.029};
     var map = new google.maps.Map(
-        document.getElementById('map'), {zoom: 14, center: uluru,});
-    // The marker, positioned at Uluru
-    var marker = new google.maps.Marker({position: uluru, map: map});
+        document.querySelector('.map'), {zoom: 14, center: lviv,});
+    var marker = new google.maps.Marker({position: lviv, map: map});
   }
